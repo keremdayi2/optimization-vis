c6026df175741d833b8ea83493b8d1def68bc0b0
Kerem Dayi <kerem@dhcp-10-250-165-97.harvard.edu>
2023-06-08 11:06:39 -0400

    add push pull with pytorch

--- QuadraticProgram.py
+++ QuadraticProgram.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class QuadraticProgam:
+class QuadraticProgram:
     def __init__(self):
-        self.objective = []
-        self.ineq = []
-        self.eq = []
+        self.objective = [] # P, q, r
+        self.ineq = [] # tuples (ci,di)
+        self.eq = [] # tuples (ai, bi)
 
     def add_objective(self):
         pass
 
-    def add_equality_constraint(self):
+    def add_equality_constraint(self, expression):
         raise NotImplementedError
 
-    def add_inequality_constraint(self, constraint):
+    def add_inequality_constraint(self, expression):
         raise NotImplementedError
 
     
